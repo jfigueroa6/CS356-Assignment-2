2e994af0e7576217e5d3eba5ff17a36d9cd5e3e2
Jose <jose.s.figueroa6@gmail.com>
2014-11-19 09:28:41 -0800

    Created three Visitor classes to count the number of users, groups, and total news feed messages.

--- src/edu/cs356/assignment2/gui/ControlPanel.java
+++ src/edu/cs356/assignment2/gui/ControlPanel.java
@@ -56,6 +56,7 @@ public class ControlPanel extends JPanel{
 		//Initialize Show portion
 		initShowButtons();
 	}
+	
 	//=========================================================
 	// Methods
 	//=========================================================
