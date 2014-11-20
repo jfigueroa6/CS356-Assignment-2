ad386ef54bdb6977995d0dc3323a76890bc70381
Jose <jose.s.figueroa6@gmail.com>
2014-11-19 20:06:15 -0800

    Implemented the ActionListener for the openUserView button. Also added methods to allow for the update and removal of opened views.

--- src/edu/cs356/assignment2/gui/ControlPanel/ControlPanelOpenUser.java
+++ src/edu/cs356/assignment2/gui/ControlPanel/ControlPanelOpenUser.java
@@ -3,19 +3,24 @@ package edu.cs356.assignment2.gui.ControlPanel;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import edu.cs356.assignment2.gui.AdminControlPanel;
+import edu.cs356.assignment2.gui.UserView.UserView;
 import edu.cs356.assignment2.service.TwitterService;
+import edu.cs356.assignment2.service.TwitterUser.User;
 
 @SuppressWarnings("serial")
 public class ControlPanelOpenUser extends JPanel {
 	private static ControlPanelOpenUser instance = null;	/**Holds static reference to an instance of this class*/
-	private static List<UserView> openUserViews = null;
+	private List<UserView> openUserViews = null;		/**Holds a reference to a list of all open user views*/
 	
 	private JButton openUserView = new JButton("Open User View");
 	
@@ -27,6 +32,7 @@ public class ControlPanelOpenUser extends JPanel {
 	private ControlPanelOpenUser() {
 		acpSingleton = AdminControlPanel.getInstance();
 		service = TwitterService.getInstance();
+		openUserViews = new LinkedList<UserView>();
 		
 		setBackground(getBackground());
 		setPreferredSize(new Dimension(AdminControlPanel.WIDTH / 2,
@@ -50,7 +56,13 @@ public class ControlPanelOpenUser extends JPanel {
 		openUserView.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				//TODO:Implement this
+				User user = service.getUser(acpSingleton.getSelectedID());
+				//If the ID is a user, then a user object is stored in user so create a new window.
+				//Else show an error.
+				if (user != null)
+					openUserViews.add(new UserView(user));
+				else
+					JOptionPane.showMessageDialog(acpSingleton, "Selected ID is not a User.", "User ID Error", JOptionPane.ERROR_MESSAGE);
 			}
 		});
 	}
@@ -73,5 +85,21 @@ public class ControlPanelOpenUser extends JPanel {
 		add(Box.createRigidArea(new Dimension(0, 5)));
 		add(openUserView);
 		add(Box.createRigidArea(new Dimension(0, getPreferredSize().height - 50)));
+	}
+	
+	/**
+	 * Remove a view from the openUserViews list. Called when the UserView is disposed.
+	 * @param view	View to remove.
+	 */
+	public void removeOpenView(UserView view) {
+		openUserViews.remove(view);
+	}
+	
+	/**
+	 * Refreshes any open user views to display any new messages for their followers.
+	 */
+	public void updateOpenViews() {
+		for (UserView view : openUserViews)
+			view.repaint();
 	}
 }
