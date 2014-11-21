00fca705f1ac6055dc6f47dfecb70c0dd3630f1b
Jose <jose.s.figueroa6@gmail.com>
2014-11-21 08:33:36 -0800

    Finished implementing the FollowPanel class. A Listener was added to the unfollow button which takes a selectedID from the following list, and the unfollow method of TwitterService is called. The selectedID is set by a list listener which is triggered when a list item is selected.

--- src/edu/cs356/assignment2/gui/UserView/FollowPanel.java
+++ src/edu/cs356/assignment2/gui/UserView/FollowPanel.java
@@ -12,6 +12,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import edu.cs356.assignment2.service.TwitterService;
 import edu.cs356.assignment2.service.TwitterUser.User;
@@ -40,6 +42,7 @@ public class FollowPanel extends JPanel {
 		
 		//Initialize ActionListeners
 		addFollowUserListener();
+		addUnfollowUserListener();
 		
 		//Initialize the first following list
 		updateFollowingList();	
@@ -54,6 +57,22 @@ public class FollowPanel extends JPanel {
 	//=========================================================
 	// Methods
 	//=========================================================
+	/**
+	 * Adds a ListSelectionListener to the given JList list. This listener changes
+	 * selectedID to the selected list item. This is used as a precursor to unfollowing
+	 * a user.
+	 * @param list	List to add Listener to.
+	 */
+	private void addFollowingListListener(JList<String> list) {
+		list.addListSelectionListener(new ListSelectionListener() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				selectedID = ((JList<String>)arg0.getSource()).getSelectedValue();
+			}	
+		});
+	}
+	
 	/**
 	 * Adds an ActionListener to the followUser button. This will take a user ID from
 	 * the userID JTextArea, and the TwitterService addFollowing method will be called.
@@ -76,6 +95,29 @@ public class FollowPanel extends JPanel {
 	}
 	
 	/**
+	 * Adds an ActionListener to the unfollowUser button. This will take a user ID selected from the following list
+	 * and the TwitterService unfollowUser will be called. If this method is successful the following list will be
+	 * updated to show the change, and the selected ID will be set back to null. Else, a dialog message will be display
+	 * giving the user some info on how to unfollow another user.
+	 */
+	private void addUnfollowUserListener() {
+		unfollowUser.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//First make sure an ID was selected. Then if the unfollowUser method from the service succeeds, refresh the following list
+				//else display a dialog window
+				if (selectedID != null && service.unfollow(selectedID, user.getID())) {
+					changedFollowing();
+					selectedID = null;		//No ID selected so set it to null
+				}
+				else
+					JOptionPane.showMessageDialog(following, "A user ID must be selected from the list.",
+							"Unfollow User Info", JOptionPane.INFORMATION_MESSAGE);	
+			}	
+		});
+	}
+	
+	/**
 	 * Called when a change to following users has happened. For example, followng or unfollowing a user.
 	 */
 	private void changedFollowing() {
@@ -91,11 +133,14 @@ public class FollowPanel extends JPanel {
 	 * Updates the following list to show any changes to the users being followed.
 	 */
 	private void updateFollowingList() {
-		
+		//Setup the list by copying the following IDs from user
 		JList<String> list = new JList<String>(Arrays.copyOf(user.getFollowing().toArray(), user.getFollowing().size(), String[].class));	//(String[])user.getFollowing().toArray(new String[user.getFollowing().size()])
 		list.setSelectionMode(1);	//Allow only one item to be selected
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setPreferredSize(new Dimension(getWidth(), getHeight() * 2/3));
+		//Add the listener to the list
+		addFollowingListListener(list);
+		//Add scrollbars
 		following = new JScrollPane(list);
 	}
 }
